f7aed79a7bb42e479e68078225aa92e5a227b1e9
ManolacheClaudiu <79422343+ManolacheClaudiu@users.noreply.github.com>
2022-10-21 00:14:46 +0300

    Add files via upload
    dupa tutorial linkedlin

---
+++ smartnotes/notes/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseRedirect
+
+from .forms import NotesForm
+from .models import Notes
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+class NotesCreateView(CreateView):
+    model = Notes
+    # fields = ['title', 'text']
+    success_url = '/smart/notes'
+    form_class =  NotesForm
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit =False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+# Create your views here.
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html',  {'notes':all_notes})
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist")
+    return render(request, 'notes/notes_detail.html', {'note':note})
